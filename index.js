4887da8b668cdce4ca7c81d6b9bb2b6c34c28de4
Navita Ramasar <navitaramasar14@gmail.com>
2023-08-22 17:40:20 -0400

    farelab doubler

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Code your solution in this file!
+// const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+
+// function returnFirstTwoDrivers() {
+//     return drivers.slice(0,2)
+// }
+
+
+// function returnLastTwoDrivers() {
+//     return drivers.slice(2)
+// }
+
+// const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+
+
+
+// function createFareMultiplier(i = 0) {
+//     return 
+// }
+
+
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = function (fareMultiplier) {  
+  return function(value) {
+    return fareMultiplier * value;
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+//fareDOuble(fare)
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers,returnLastTwoDrivers){
+    return returnLastTwoDrivers(arrayOfDrivers)
+}
